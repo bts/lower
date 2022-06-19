bee41a714b91ac4f830eec81732cc92ed7f4b11d
Brian Schroeder <bts@gmail.com>
2022-06-19 19:47:31 -0400

    Switch to a flake setup with override support

--- flake.nix
+++ flake.nix
@@ -1,42 +1,70 @@
 {
-  description = "Lowering a surface syntax into different intermediate representations";
-
+  description =
+    "Lowering a surface syntax into different intermediate representations";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    np.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    fu.url = "github:numtide/flake-utils";
+
+    # Flake
+    #shs.url = "github:luc-tielen/souffle-haskell/c46d0677e4bc830df89ec1de2396c562eb9d86d3";
+
+    # Non-flake
+    #alga.url = "github:snowleopard/alga/75de41a4323ab9e58ca49dbd78b77f307b189795";
+    #alga.flake = false;
   };
+  outputs = { self, np, fu, ... }@inputs:
+    with np.lib;
+    with fu.lib;
+    eachSystem [ "x86_64-linux" "aarch64-darwin" ] (system:
+      let
+        ghcVersion = "902";
+        config = {
+          allowBroken = true;
+        };
+        overlay = final: _:
+          let
+            haskellPackages =
+              final.haskell.packages."ghc${ghcVersion}".override {
+                overrides = hf: hp: # final, previous
+                  with final.haskell.lib; rec {
+                    # Examples:
+                    #dependent-hashmap = with hf; unmarkBroken (dontCheck hp.dependent-hashmap);
+                    #haddock-api = with hf; doJailbreak (callHackage "haddock-api" "2.21.0" {}); # From Hackage
+                    #algebraic-graphs = with hf; dontCheck (callCabal2nix "algebraic-graphs" (inputs.alga) { }); # Non-flake input
+                    #inherit (shs.packages."${system}") souffle-haskell; # Flake input
 
-  outputs = { self, nixpkgs }:
-    let
-      packageName = "lower";
-      supportedSystems = [ "x86_64-linux" "aarch64-darwin" ];
-      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
-      nixpkgsFor = forAllSystems (system: import nixpkgs {
-        inherit system;
-        overlays = [ self.overlay ];
-      });
-    in
-    {
-      overlay = (final: prev: {
-        ${packageName} = final.haskellPackages.callCabal2nix packageName ./. {};
-      });
-
-      packages = forAllSystems (system: {
-        ${packageName} = nixpkgsFor.${system}.${packageName};
-      });
-
-      defaultPackage = forAllSystems (system: self.packages.${system}.${packageName});
-
-      checks = self.packages;
-
-      devShell = forAllSystems (system: let haskellPackages = nixpkgsFor.${system}.haskellPackages;
-        in haskellPackages.shellFor {
-          packages = p: [self.packages.${system}.${packageName}];
-          withHoogle = false;
-          buildInputs = with haskellPackages; [
-            haskell-language-server
-            ghcid
-            cabal-install
+                    lower = with hf; (callCabal2nix "lower" ./. { });
+                  };
+              };
+          in { inherit haskellPackages; };
+        pkgs = import np {
+          inherit system config;
+          overlays = [
+            #shs.overlay."${system}" # Flake
+            overlay
           ];
-        });
-  };
+        };
+
+        haskellPackages = pkgs.haskellPackages;
+
+      in with pkgs.lib; rec {
+        inherit overlay;
+
+        packages = { inherit (haskellPackages) lower; };
+
+        defaultPackage = packages.lower;
+
+        devShell = haskellPackages.shellFor {
+          withHoogle = false; # provides docs; optional.
+          packages = p: [
+            p.lower
+          ];
+          buildInputs = with haskellPackages; [
+            cabal-install
+            ghcid
+            haskell-language-server
+            hlint
+          ];
+        };
+      });
 }
