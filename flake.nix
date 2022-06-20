1003d2210cda4d7be42fe214d4d7b1e2ab315ddc
Brian Schroeder <bts@gmail.com>
2022-06-20 10:41:47 -0400

    Clean up flake.nix

--- flake.nix
+++ flake.nix
@@ -4,13 +4,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-
-    # Flake
-    #shs.url = "github:luc-tielen/souffle-haskell/c46d0677e4bc830df89ec1de2396c562eb9d86d3";
-
-    # Non-flake
-    #alga.url = "github:snowleopard/alga/75de41a4323ab9e58ca49dbd78b77f307b189795";
-    #alga.flake = false;
   };
   outputs = { self, nixpkgs, flake-utils, ... }@inputs:
     with nixpkgs.lib;
@@ -30,13 +23,6 @@
               final.haskell.packages."ghc${ghcVersion}".override {
                 overrides = hf: hp: # final, previous
                   with final.haskell.lib; rec {
-                    # Examples:
-                    #dependent-hashmap = with hf; unmarkBroken (dontCheck hp.dependent-hashmap);
-                    #haddock-api = with hf; doJailbreak (callHackage "haddock-api" "2.21.0" {}); # From Hackage
-                    #BinderAnn = with hf; callHackageDirect { pkg = "BinderAnn"; ver = "0.1.0.0"; sha256 = "..."; } {}; # Works for *any* version released on Hackage, not only those before our version of all-cabal-hashes.
-                    #algebraic-graphs = with hf; dontCheck (callCabal2nix "algebraic-graphs" (inputs.alga) { }); # Non-flake input
-                    #inherit (shs.packages."${system}") souffle-haskell; # Flake input
-
                     ${packageName} = with hf; (callCabal2nix packageName ./. { });
                   };
               };
@@ -44,7 +30,6 @@
         pkgs = import nixpkgs {
           inherit system config;
           overlays = [
-            #shs.overlay."${system}" # Flake
             overlay
           ];
         };
@@ -58,7 +43,6 @@
         defaultPackage = packages.${packageName};
 
         devShell = haskellPackages.shellFor {
-          withHoogle = false; # provides docs; optional.
           packages = p: [
             p.${packageName}
           ];
